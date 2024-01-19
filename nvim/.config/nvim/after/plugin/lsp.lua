82a0c34a361fcbc588821f98e08db97e1fa04ee2
GuisandeGolfer <DGuisande873@gmail.com>
2024-01-19 08:31:00 -0800

    add changes from Mac for Linux

---
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,16 @@
+local lsp = require('lsp-zero')
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'eslint', 'rust_analyzer', 'sumneko_lua'},
+  handlers = {
+    lsp.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
+})
+
+lsp.setup()
+
