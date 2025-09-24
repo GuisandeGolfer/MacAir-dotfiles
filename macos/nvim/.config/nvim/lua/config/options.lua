f97e393bac004d441364b1016ae52642d34ef657
GuisandeGolfer <DGuisande873@gmail.com>
2025-09-24 11:47:09 -0700

    reset lazy.vim, adding back settings slowly

--- macos/nvim/.config/nvim/lua/config/options.lua
+++ macos/nvim/.config/nvim/lua/config/options.lua
@@ -1,21 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("i", "jk", "<Esc>")
-
-vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
-
-vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically, <space>s-plit v-ertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally, <space>s-plit h-orizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width, <space>s-plit e-qually
-vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
-vim.keymap.set("n", "<leader>sm", "<C-w>|") -- maximize current split window width
-
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- T-ab O-pen:open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- T-ab X-close: close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>") -- T-ab N-ext: go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>") -- T-ab P-revious: go to previous tab
-
--- taking screenshots
---
-vim.keymap.set("v", "<leader>cn", ":CarbonNow<CR>", { silent = true })
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
